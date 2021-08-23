531bec7a04fb74c418c417f0b3b02e91ab3890e5
Armin Novak <armin.novak@thincast.com>
2021-08-23 13:53:58 +0200

    Added assertions

--- libfreerdp/core/server.c
+++ libfreerdp/core/server.c
@@ -1315,6 +1315,8 @@ BOOL WINAPI FreeRDP_WTSVirtualChannelClose(HANDLE hChannelHandle)
 	rdpPeerChannel* channel = (rdpPeerChannel*)hChannelHandle;
 	BOOL ret = TRUE;
 
+	WINPR_ASSERT(channel);
+
 	if (channel)
 	{
 		WTSVirtualChannelManager* vcm = channel->vcm;
